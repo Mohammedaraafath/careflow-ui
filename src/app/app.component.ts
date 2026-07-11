dce35887c7decd0cae62df8a7f5e507ec6b93981
araafath <araafa786@gmail.com>
2026-07-11 18:52:45 +0530

    home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
+
+import { HomeComponent } from './features/home/home.component';
+import { DepartmentsComponent } from './features/departments/departments.component';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +13,8 @@ import { FooterComponent } from './layout/footer/footer.component';
   imports: [
     RouterOutlet,
     NavbarComponent,
+    HomeComponent,
+    DepartmentsComponent,
     FooterComponent
   ],
   templateUrl: './app.component.html',
