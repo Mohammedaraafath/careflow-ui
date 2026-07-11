386556a070b507d3304276833d012c94a6dc9e67
araafath <araafa786@gmail.com>
2026-07-11 18:17:00 +0530

    home page

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,11 +1,31 @@
+// import { Component } from '@angular/core';
+// import { RouterLink } from '@angular/router';
+//
+// @Component({
+//   selector: 'app-navbar',
+//   standalone: true,
+//   imports: [RouterLink],
+//   templateUrl: './navbar.component.html',
+//   styleUrl: './navbar.component.css'
+// })
+// export class NavbarComponent {}
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+}
