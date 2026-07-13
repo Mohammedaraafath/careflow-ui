2430903611aade35e6218edd60ccea4fd04cc748
araafath <araafa786@gmail.com>
2026-07-13 10:56:39 +0530

    hotel home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -9,8 +9,12 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 showContact = false;
 showAppointment = false;
+// bookNow() {
+//   window.open('https://www.airbnb.com/', '_blank');
+// }
 bookNow() {
-  window.open('https://www.airbnb.com/', '_blank');
+  window.location.href =
+    'https://www.airbnb.co.in/rooms/1584710333921147469';
 }
 
   }
